5332bee9a569536a8275ddebdf5caf7d856aa992
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-08-11 23:53:20 +0300

    Create wine_quality_model.py

---
+++ wine-quality-prediction/wine_quality_model.py
@@ -0,0 +1,9 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv('dataset.csv')
+
